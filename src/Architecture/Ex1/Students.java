595ec9e43e44d5dcb29bf89d120b522d6bf79acf
roee-tzarom <roeetzarum2002@gmail.com>
2026-08-26 15:32:13 +0300

    Refactor Students class by removing unused methods
    Adds comments only; behavior is unchanged.

--- src/Architecture/Ex1/Students.java
+++ src/Architecture/Ex1/Students.java
@@ -1,12 +1,20 @@
+// -----------------------------------------------------------------------------
+// Student-domain model used by the architecture exercise. It groups the student
+// data and the operations that operate on that data in one small abstraction.
+// -----------------------------------------------------------------------------
+
 package Architecture.Ex1;
 import java.io.*;
+
 
 public class Students {
     private int[] studentsArray;
 
+
     public int[] insertGrades(String fileName) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         String line;
+
 
         int count = 0;
         while ((line = br.readLine()) != null) {
@@ -14,16 +22,20 @@ public class Students {
         }
         br.close();
 
+
         String[] grades = new String[count];
         br = new BufferedReader(new FileReader(fileName));
         int index = 0;
+
 
         while ((line = br.readLine()) != null) {
             grades[index++] = line;
         }
         br.close();
 
+
         studentsArray = new int[grades.length];
+
 
         for (int i = 0; i < grades.length; i++) {
             String studentGrades = grades[i];
@@ -34,34 +46,3 @@ public class Students {
             int grade4 = Integer.parseInt(parts[3]);
             studentsArray[i] = (grade4 << 24) | (grade3 << 16) | (grade2 << 8) | grade1;
         }
-
-        return studentsArray;
-    }
-    public void displayHex() {
-        for (int num : studentsArray) {
-            System.out.printf("%08X", num);
-        }
-        System.out.println();
-    }
-    public int getStudentExam(int i, int j) {
-        return (studentsArray[i - 1] >> (8 * (j - 1))) & 0xFF;
-    }
-    public void setStudentExam(int i, int j, int k) {
-        int shift = 8 * (j - 1);
-        studentsArray[i - 1] = (studentsArray[i - 1] & ~(0xFF << shift)) | ((k & 0xFF) << shift);
-    }
-    public float averageStudent(int i) {
-        int sum = 0;
-        for (int j = 1; j <= 4; j++) {
-            sum += getStudentExam(i, j);
-        }
-        return (float) sum/4;
-    }
-    public float averageExam(int j) {
-        int sum = 0;
-        for (int i = 1; i <= studentsArray.length; i++) {
-            sum += getStudentExam(i, j);
-        }
-        return sum/(float)studentsArray.length;
-    }
-}
